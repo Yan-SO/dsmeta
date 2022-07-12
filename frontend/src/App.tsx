e1948153fd5cf3a31586c77e97c2de48e4037e55
Yan Silva de Oliveira <yanoliveira2001@gmail.com>
2022-07-12 07:56:01 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton/index"
 
 function App() {
   return(
-    <h1>ola mundo</h1>
+    <>
+      <h1>ola mundo</h1>
+      <NotificationButton/>
+      <NotificationButton/>
+    </>
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/Notificaçao.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btm">
+            <img src={icon} alt="Notification" />
+         </div>
+    )
+}
+
+export default NotificationButton
